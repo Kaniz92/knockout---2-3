274a38ca61b9ea900d5c6f33ab0df3ed8322d7b1
Kaniz92 <kanishkasilva22@gmail.com>
2018-03-02 16:49:29 +0530

    init

---
+++ OrgEmployee/OrgEmployee/Scripts/ViewModels/Enrollment/CreateVM.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    ko.applyBindings(CreateVM);
+});
+
+var CreateVM = {
+    Band: ko.observable(),
+    Departments: ko.observableArray([]),
+    Department: ko.observable(),
+    Employees: ko.observableArray([]),
+    employee: ko.observable(),
+
+    SaveEnrollment: function () {
+        $.ajax({
+            url: '/Enrollment/Create',
+            type: 'post',
+            dataType: 'json',
+            data: ko.toJSON(this),
+            contentType: 'application/json',
+            success: function (result) {
+            },
+            error: function (err) {
+                console.log('Error: ' + err);
+            },
+            complete: function () {
+                window.location.href = '/Enrollment/Index';
+            }
+        });
+    }
+
+};
